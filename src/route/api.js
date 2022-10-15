8f6f6c92180117e0b8915355dbbb864be257d488
tam <vuthitam14082001@gmail.com>
2022-10-15 16:37:35 +0700

    product

---
+++ src/route/api.js
@@ -0,0 +1,12 @@
+import express from "express";
+import userController from "../controllers/userController";
+
+let route = express.Router();
+const initAPIRoute = (app) => {
+  route.post('/api/login', userController.handleLogin);
+  route.post('/api/register', userController.register);
+
+  return app.use('/', route);
+}
+
+export default initAPIRoute;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import configViewEngine from './config/viewEngine';
-import initWebRoute from './route/web'
+import initAPIRoute from './route/api'
 import connection from './config/connectDB';
 
 require('dotenv').config();
@@ -9,14 +9,19 @@ const app = express();
 let port = process.env.PORT || 8000;
 
 //body parser
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // setup view engine
 configViewEngine(app);
 
 // init web route
-initWebRoute(app);
+initAPIRoute(app);
+
+// handle 404 not found
+// app.use((req, res) => {
+//   return res.send('404.ejs')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
