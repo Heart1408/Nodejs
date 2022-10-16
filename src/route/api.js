f7cea3f4b41d321d87d5c46d98b4cd4ba760ebf3
ttchan1521 <tranthutrang15012001@gmail.com>
2022-10-16 22:27:01 +0700

    order feature

--- src/route/api.js
+++ src/route/api.js
@@ -1,6 +1,8 @@
 import express from "express";
 import userController from "../controllers/userController";
 import productController from '../controllers/productController';
+import orderController from '../controllers/orderController';
+import orderDetailController from '../controllers/orderDetailController';
 import verifyToken from '../middleware/auth';
 
 let route = express.Router();
@@ -12,6 +14,13 @@ const initAPIRoute = (app) => {
   route.get('/api/product/filter', productController.filter);
   route.get('/api/getInfoProduct/:id', verifyToken, productController.getInfoProduct);
   route.get('/api/addProductToCart/:id', verifyToken, productController.addProductToCart);
+
+  route.get('/api/getAllOrder', orderController.getAllOrder);
+  route.post('/api/changeStatusOrder', orderController.changeStatusOrder);
+  route.post('/api/deleteOrder', orderController.deleteOrder);
+  route.get('/api/detailOrder/:id', orderDetailController.getDetailOrder);
+  route.get('/api/orderHistory', verifyToken, orderController.orderHistory);
+  route.post('/api/createOrder', verifyToken, orderDetailController.createOrder); 
   return app.use('/', route);
 }
 
