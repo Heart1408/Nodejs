0fd39e1cf2afe946a381b857ee9d09fe93e8cf72
trangtt-monster <trangtt@monster.io>
2022-10-19 17:44:11 +0700

    total sold product

--- src/models/orderdetail.js
+++ src/models/orderdetail.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      OrderDetail.belongsTo(models.Product, {foreignKey: 'product_id'})
     }
   }
   OrderDetail.init({
--- src/services/orderDetailService.js
+++ src/services/orderDetailService.js
@@ -1,4 +1,5 @@
-import db from '../models/index'
+import { ModulesOption } from '@babel/preset-env/lib/options';
+import db, { sequelize } from '../models/index'
 
 let getDetailOrder = (orderId) => {
     return new Promise(async(resolve, reject) => {
@@ -68,7 +69,67 @@ let createOrder = (data, userId) => {
     })
 }
 
+let getAmountSoldProducts = () => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            
+            let productList = await db.Product.findAll({
+                attributes: ['id', 'name', 'description', 'image', 'amount', 'price', [db.sequelize.fn('sum', db.sequelize.col('OrderDetails.amount')), 'total_sold']],
+                include: [
+                    {
+                        model: db.OrderDetail,
+                        required: true,
+                        attributes: []
+                    }
+                ],
+                group: ['Product.id'],
+                raw: true
+            })
+
+            productList.forEach(element => {
+                element.total_sold = parseInt(element.total_sold)
+            });
+            
+
+            /** 
+            let productList = await sequelize.query("select product_id, P.name, sum(OrderDetails.amount) as sum from OrderDetails join Products P on P.id = OrderDetails.product_id GROUP BY product_id",
+                {type: sequelize.QueryTypes.SELECT});
+            
+            
+            let productList = await db.Product.findAll({
+                raw: true
+            });
+            for (let i=0; i<productList.length; i++)
+            {
+                productList[i].totalSold = await db.OrderDetail.sum('amount', {
+                    where: {
+                        product_id: productList[i].id
+                    }
+                })
+                console.log(productList[i].totalSold);
+            }
+            */
+            
+            if (productList) {
+                console.log(productList);
+                resolve({
+                    success: true,
+                    productList: productList
+                })
+            }
+            else {
+                resolve({
+                    success: false
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    }) 
+}
+
 module.exports = {
     getDetailOrder: getDetailOrder,
-    createOrder: createOrder
+    createOrder: createOrder,
+    getAmountSoldProducts: getAmountSoldProducts
 }
